bf0bcb46b563946526153e1950b4e62e129836bc
limikmag <limikmag@gmail.com>
2019-03-28 08:20:46 +0100

    Update docker_troubleshooting.sh

--- docker_troubleshooting.sh
+++ docker_troubleshooting.sh
@@ -37,3 +37,21 @@ $ sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.1/ctop-0.7.1-l
 $ sudo chmod +x /usr/local/bin/ctop
 
 $ dmesg - program which print logs from kernel ( when some process in container are killed we can take a look to this logs)
+
+Monitoring Resource Consumption
+The systemd-cgls command provides a static snapshot of the cgroup hierarchy. To see a dynamic account of currently running cgroups ordered by their resource usage (CPU, Memory, and IO), use:
+$ systemd-cgtop -p
+e.g. output
+/                                                                                                        110   38.3     6.1G        -        -
+/docker                                                                                                    -    3.6     7.4G        -        -
+/docker/8211c306daf29bd1241c004ac530bf45d334b29f3678d1e59d70795c43abd581                                  18    3.3   674.9M        -        -
+/docker/9680e059657f8a82f770ec46b2a8659696e220df8a4a719d12ad3a0d28196955                                   5    0.3   506.1M        -        -
+/kubepods                                                                                                  -    7.8     1.1G        -        -
+/kubepods/besteffort                                                                                       -    1.3   538.8M        - 
+
+
+After this we can run:
+$ watch systemd-cgls /docker/1633e7ffa4a1ea4887d997941d7b02512cd3f5b2dc3ba7ce30d1a705a248839d
+
+And we will see all processes running in container
+
